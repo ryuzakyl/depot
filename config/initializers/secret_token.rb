83130d2fa0d2bf83d66c19ba04f62dd0128e335a
John Morris <jmorrispratt@gmail.com>
2014-01-10 19:18:52 -0800

    created the rails application that will hold all the samples of the book "Pragmatic.Agile.Web.Development.with.Rails.4.Sep.2013"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'a459f45ff239db3d4474fcba1429df22fd3baa0c1fe2c02ecb8fd02783334a836008b6fb8bcee3f628d92fa90f757475f57c41f989caeae3cfc2e7c6c972b08a'
